3437844e0684cd8c31983e3f13536856268be9c0
FilippoLucchesi <f.lucchesi14@studenti.unipi.it>
2024-01-23 15:35:55 +0100

    search companies

--- gamecritic/src/main/java/it/unipi/gamecritic/repositories/CustomCompanyRepository.java
+++ gamecritic/src/main/java/it/unipi/gamecritic/repositories/CustomCompanyRepository.java
@@ -1,10 +1,11 @@
 package it.unipi.gamecritic.repositories;
 
 import com.mongodb.DBObject;
+import it.unipi.gamecritic.entities.Company;
 
 import java.util.List;
 
 public interface CustomCompanyRepository {
     List<DBObject> findByDynamicAttribute(String attributeName, String attributeValue);
-    List<DBObject> search(String query);
+    List<Company> search(String query);
 }
--- gamecritic/src/main/java/it/unipi/gamecritic/repositories/CustomCompanyRepositoryImpl.java
+++ gamecritic/src/main/java/it/unipi/gamecritic/repositories/CustomCompanyRepositoryImpl.java
@@ -2,6 +2,7 @@ package it.unipi.gamecritic.repositories;
 
 import com.mongodb.DBObject;
 import com.mongodb.client.MongoCollection;
+import it.unipi.gamecritic.entities.Company;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -25,10 +26,9 @@ public class CustomCompanyRepositoryImpl implements CustomCompanyRepository{
         return mongoTemplate.find(query, DBObject.class, "companies");
     }
     @Override
-    public List<DBObject> search(String query){
+    public List<Company> search(String query){
         Criteria criteria = Criteria.where("Name").regex(query, "i");
         Query q = new Query(criteria).limit(10);
-
-        return mongoTemplate.find(q, DBObject.class, "companies");
+        return mongoTemplate.find(q, Company.class, "companies");
     }
 }
