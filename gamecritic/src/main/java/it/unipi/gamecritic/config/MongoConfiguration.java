6ef7057ec9b1498a527b776b3c7b9c6901bbd5cb
Ettore Ricci <minecraft.etto@gmail.com>
2024-02-11 19:53:53 +0100

    Remove unused import and add null suppression

--- gamecritic/src/main/java/it/unipi/gamecritic/config/MongoConfiguration.java
+++ gamecritic/src/main/java/it/unipi/gamecritic/config/MongoConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.convert.*;
 import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
@@ -18,7 +17,9 @@ public class MongoConfiguration {
 
     @Bean
     public MappingMongoConverter mappingMongoConverter() {
+        @SuppressWarnings("null")
         DbRefResolver dbRefResolver = new DefaultDbRefResolver(mongoDbFactory);
+        @SuppressWarnings("null")
         MappingMongoConverter converter = new MappingMongoConverter(dbRefResolver, mongoMappingContext);
         converter.setTypeMapper(new DefaultMongoTypeMapper(null));
         return converter;
