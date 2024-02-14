fd63a8a56d8bb5b9a5dcf90e497e9504a639fc60
Ettore Ricci <minecraft.etto@gmail.com>
2024-02-14 19:15:35 +0100

    Fixed warning

--- gamecritic/src/main/java/it/unipi/gamecritic/controllers/CompanyController.java
+++ gamecritic/src/main/java/it/unipi/gamecritic/controllers/CompanyController.java
@@ -29,6 +29,7 @@ import jakarta.servlet.http.HttpSession;
 public class CompanyController {
     private final GameRepository gameRepository;
     private final CompanyRepository companyRepository;
+    @SuppressWarnings("unused")
     private static final Logger logger = LoggerFactory.getLogger(CompanyController.class);
     @Autowired
     public CompanyController(GameRepository gameRepository, CompanyRepository companyRepository) {
